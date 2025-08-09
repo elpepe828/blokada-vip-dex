187a0321e65ba475d58f76fba4af1374bef12c03
elpepe828 <baldoquinjonatan558@gmail.com>
2025-08-08 20:59:02 -0400

    Delete src/main/java/com/blokada/plus/ license directory

--- src/main/java/com/blokada/plus/ license/LicenseManager.java
+++
@@ -1,7 +0,0 @@
-package com.blokada.plus.license;
-
-public class LicenseManager {
-    public boolean isVipActive() { return true; }
-    public String getVipExpiry() { return "2099-12-31"; }
-    public int getVipLevel() { return 5; }
-}
