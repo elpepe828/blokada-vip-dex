79718c077fea0e6cfec957f378e7e94438cb279a
elpepe828 <baldoquinjonatan558@gmail.com>
2025-08-08 21:00:50 -0400

    Create LicenseManager.java

---
+++ src/main/java/com/blokada/plus/license/LicenseManager.java
@@ -0,0 +1,7 @@
+package com.blokada.plus.license;
+
+public class LicenseManager {
+    public boolean isVipActive() { return true; }
+    public String getVipExpiry() { return "2099-12-31"; }
+    public int getVipLevel() { return 5; }
+}
